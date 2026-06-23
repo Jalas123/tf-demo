4e3f974caf64d77bd1ab4a938f9505399e46bd60
Alexey <sambexal@gmail.com>
2026-06-23 11:08:36 +0100

    edit main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # providers + resources
 terraform {
-  required_version = ">= 1.6"
+  required_version = ">= 1.4"
   required_providers {
     aws = { source = "hashicorp/aws", version = "~> 5.0" }
   }
